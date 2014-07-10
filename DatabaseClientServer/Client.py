deef8166d284184863c4983820200a31fb346dab
muadibbm <muadibbm@10.0.0.3>
2014-07-10 16:16:46 -0400

    Database client server operational missing some features

--- DatabaseClientServer/Client.py
+++ DatabaseClientServer/Client.py
@@ -5,6 +5,7 @@ Created on 2014-07-09
 '''
 
 import socket
+import sys
 
 class Client(object):
     '''
@@ -21,6 +22,39 @@ class Client(object):
         
     def run(self):
         print self._client_socket.recv(1024)
+        while True:
+            print ("\nPython DB Menu\n" + 
+                   "1. Find customer\n" +
+                   "2. Add customer\n"+
+                   "3. Delete customer\n"+
+                   "4. Update customer age\n"+
+                   "5. Update customer address\n"+
+                   "6. Update customer phone\n"+
+                   "7. Print report\n"+
+                   "8. Exit\n")
+            client_input = raw_input('Select: ')
+            if(client_input == '1'):
+                name = raw_input('Enter Name: ')
+                self._client_socket.send("1" + name)
+                print self._client_socket.recv(1024)
+            elif(client_input == '2'):
+                pass
+            elif(client_input == '3'):
+                pass
+            elif(client_input == '4'):
+                pass
+            elif(client_input == '5'):
+                pass
+            elif(client_input == '6'):
+                pass
+            elif(client_input == '7'):
+                pass
+            elif(client_input == '8'):
+                self._client_socket.close()
+                print "Application terminated"
+                sys.exit()
+            else:
+                print "Invalid input"
         
 if  __name__ =='__main__':
     client = Client(9999)
--- DatabaseClientServer/Server.py
+++ DatabaseClientServer/Server.py
@@ -23,27 +23,33 @@ class Server(object):
         self._client_address = None
         
     def _initDatabase(self):
-        file = open("data.txt", 'r')
-        for line in file:
+        for line in open("data.txt", 'r'):
             line = line.translate(None, '\n')
             data = line.split('|', 3)
-            if(data[0].translate(None, ' ') != ''):
+            if(data[0].translate(None, ' ') != ''): # Skipping the record if the name does not exist
                 self.database[data[0]] = (data[1].translate(None, ' '), data[2], data[3])
         print "Database read from file"
         print self.database
         
     def run(self):
-        self._server_socket.listen(0)
+        self._server_socket.listen(1)
         print "Server is now running"
-        while True:
-            self._acceptConnection()
-        
+        if(self._acceptConnection()):
+            while True:
+                client_request = self._client_socket.recv(1024)
+                if(client_request[0] == '1'):
+                    if(self.database.has_key(client_request[1:])):
+                        self._sendMessage(client_request[1:] + ":" + str(self.database[client_request[1:]]))
+                    else:
+                        self._sendMessage("Customer not found")
+                    
     def _acceptConnection(self):
         self._client_socket, self._client_address = self._server_socket.accept()
         print "Accepted connection from client", self._client_socket
-        self.sendMessage("Connection accepted at server" + str(self._server_socket))
+        self._sendMessage("Connection accepted at server" + str(self._server_socket))
+        return True
         
-    def sendMessage(self, message):
+    def _sendMessage(self, message):
         self._client_socket.send(message)
    
 if  __name__ =='__main__':
