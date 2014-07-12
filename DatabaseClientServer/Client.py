b5ae50d84253af85458fb17baa6858ba3c3de175
muadibbm <muadibbm@10.0.0.3>
2014-07-12 00:51:25 -0400

    Server Client functional with read and write to file database.

--- DatabaseClientServer/Client.py
+++ DatabaseClientServer/Client.py
@@ -12,13 +12,21 @@ class Client(object):
     Client Class
     '''
 
-
     def __init__(self, port):
         '''
         Constructor
         '''
         self._client_socket = socket.socket()
         self._client_socket.connect((socket.gethostname(), port))
+        
+    def _getInput(self, promotMessage):
+        while True:
+            value = raw_input(promotMessage + ': ')
+            if(promotMessage == 'Enter Name' and value.translate(None, ' ') == ''):
+                print 'Name cannot be empty'
+            else:
+                break
+        return value
         
     def run(self):
         print self._client_socket.recv(1024)
@@ -34,24 +42,35 @@ class Client(object):
                    "8. Exit\n")
             client_input = raw_input('Select: ')
             if(client_input == '1'):
-                name = raw_input('Enter Name: ')
-                self._client_socket.send("1" + name)
+                self._client_socket.send('1#' + self._getInput('Enter Name'))
                 print self._client_socket.recv(1024)
             elif(client_input == '2'):
-                pass
+                self._client_socket.send('2#' + self._getInput('Enter Name') + '#' +
+                                         self._getInput('Enter Age') + '#' +  
+                                         self._getInput('Enter Address') + '#' + 
+                                         self._getInput('Enter Phone'))
+                print self._client_socket.recv(1024)
             elif(client_input == '3'):
-                pass
+                self._client_socket.send('3#' + self._getInput('Enter Name'))
+                print self._client_socket.recv(1024)
             elif(client_input == '4'):
-                pass
+                self._client_socket.send('4#' + self._getInput('Enter Name') + '#' +
+                                         self._getInput('Enter Age'))
+                print self._client_socket.recv(1024)
             elif(client_input == '5'):
-                pass
+                self._client_socket.send('5#' + self._getInput('Enter Name') + '#' +
+                                         self._getInput('Enter Address'))
+                print self._client_socket.recv(1024)
             elif(client_input == '6'):
-                pass
+                self._client_socket.send('6#' + self._getInput('Enter Name') + '#' +
+                                         self._getInput('Enter Phone'))
+                print self._client_socket.recv(1024)
             elif(client_input == '7'):
                 pass
             elif(client_input == '8'):
+                self._client_socket.send('8#')
                 self._client_socket.close()
-                print "Application terminated"
+                print "Application terminated - Goodbye!"
                 sys.exit()
             else:
                 print "Invalid input"
--- DatabaseClientServer/Server.py
+++ DatabaseClientServer/Server.py
@@ -15,33 +15,49 @@ class Server(object):
         '''
         Constructor
         '''
-        self.database = {}
-        self._initDatabase()
+        self._database = {}
+        self._init_database()
         self._server_socket = socket.socket()
         self._server_socket.bind((socket.gethostname(), port))
         self._client_socket = None
         self._client_address = None
         
-    def _initDatabase(self):
-        for line in open("data.txt", 'r'):
+    def _init_database(self):
+        dataFile = open("data.txt", 'r')
+        for line in dataFile:
             line = line.translate(None, '\n')
             data = line.split('|', 3)
             if(data[0].translate(None, ' ') != ''): # Skipping the record if the name does not exist
-                self.database[data[0]] = (data[1].translate(None, ' '), data[2], data[3])
-        print "Database read from file"
-        print self.database
+                self._database[data[0]] = (data[1].translate(None, ' '), data[2], data[3])
+        dataFile.close()
+        print "_database read from file"
+        print self._database
         
     def run(self):
-        self._server_socket.listen(1)
         print "Server is now running"
-        if(self._acceptConnection()):
-            while True:
-                client_request = self._client_socket.recv(1024)
-                if(client_request[0] == '1'):
-                    if(self.database.has_key(client_request[1:])):
-                        self._sendMessage(client_request[1:] + ":" + str(self.database[client_request[1:]]))
-                    else:
-                        self._sendMessage("Customer not found")
+        while True:
+            self._server_socket.listen(1)
+            if(self._acceptConnection()):
+                while True:
+                    client_request = self._client_socket.recv(1024)
+                    values = client_request.split('#')
+                    if(values[0] == '1'):
+                        self._findCustomer(values[1])
+                    elif(values[0] == '2'):
+                        self._addCustomer(values[1], values[2], values[3], values[4])
+                    elif(values[0] == '3'):
+                        self._deleteCustomer(values[1])
+                    elif(values[0] == '4'):
+                        self._updateCustomerAge(values[1], values[2])
+                    elif(values[0] == '5'):
+                        self._updateCustomerAddress(values[1], values[2])
+                    elif(values[0] == '6'):
+                        self._updateCustomerPhone(values[1], values[2])
+                    elif(values[0] == '7'):
+                        pass
+                    elif(values[0] == '8'):
+                        break
+                    self._updateDatabase()
                     
     def _acceptConnection(self):
         self._client_socket, self._client_address = self._server_socket.accept()
@@ -51,6 +67,56 @@ class Server(object):
         
     def _sendMessage(self, message):
         self._client_socket.send(message)
+    
+    def _updateDatabase(self):
+        dataFile = open("data.txt", 'w')
+        for entry in self._database:
+            dataFile.write(entry + '|' + self._database[entry][0] + '|' + self._database[entry][1] + '|' + self._database[entry][2] + '\n')
+        dataFile.close()
+            
+    def _findCustomer(self, name):
+        if(self._database.has_key(name)):
+            self._sendMessage(name + ":" + str(self._database[name]))
+        else:
+            self._sendMessage("Customer not found")
+            
+    def _addCustomer(self, name, age, address, phone):
+        if(self._database.has_key(name) == False):
+            self._database[name] = (age, address, phone)
+            self._sendMessage("Customer " + name + " has been added")
+        else:
+            self._sendMessage("Customer already exists")
+    
+    def _deleteCustomer(self, name):
+        if(self._database.has_key(name)):
+            self._database.pop(name)
+            self._sendMessage("Customer " + name + " has been deleted")
+        else:
+            self._sendMessage("Customer does not exist")
+    
+    def _updateCustomerAge(self, name, age):
+        if(self._database.has_key(name)):
+            self._database[name] = (age, self._database[name][1], self._database[name][2])
+            self._sendMessage("Customer " + name + "'s age has been updated to " + age)
+        else:
+            self._sendMessage("Customer not found")
+    
+    def _updateCustomerAddress(self, name, address):
+        if(self._database.has_key(name)):
+            self._database[name] = (self._database[name][0], address, self._database[name][2])
+            self._sendMessage("Customer " + name + "'s address has been updated to " + address)
+        else:
+            self._sendMessage("Customer not found")
+    
+    def _updateCustomerPhone(self, name, phone):
+        if(self._database.has_key(name)):
+            self._database[name] = (self._database[name][0], self._database[name][1], phone)
+            self._sendMessage("Customer " + name + "'s phone has been updated to " + phone)
+        else:
+            self._sendMessage("Customer not found")
+    
+    def _printReport(self):
+        pass
    
 if  __name__ =='__main__':
     server = Server(9999)
